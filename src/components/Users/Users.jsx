da7ac21174340843bf6513a7440341a3dc6998df
oilprint <irynakozlova.k@gmail.com>
2024-01-30 22:27:17 +0200

    hooks

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,17 +4,13 @@ import axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-      axios.get('https://social-network.samuraijs.com/api/1.0/users')
+  componentDidMount() {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
       .then(response => { 
         this.props.setUsers(response.data.items)
       });
-    
-  }
 
+  }
   render() {
     return (
     <div>
